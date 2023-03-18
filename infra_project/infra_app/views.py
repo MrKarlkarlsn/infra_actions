4d702d9e8f646f41658439cd300a176e22ff23a6
Кузьмин Артём <mr.KarlKarlsn@outlook.com>
2023-03-18 16:50:51 +0300

    Создан файл конфигурации

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -4,9 +4,5 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('У меня получилось!')
 
-
 def second_page(request):
-    return HttpResponse('А это вторая страница')
-
-def second_page1(request):
     return HttpResponse('А это вторая страница')
